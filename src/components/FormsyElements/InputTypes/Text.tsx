c45ac9539429e6cc987149746e49153243dac9c8
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-16 21:22:22 +0300

    Added formsy text input component

---
+++ src/components/FormsyElements/InputTypes/Text.tsx
@@ -0,0 +1,65 @@
+import React, { FunctionComponent } from 'react';
+import { TextField } from '@material-ui/core';
+import { withFormsy, FormsyInjectedProps } from 'formsy-react';
+
+type InputValueType = string | number;
+
+interface Props extends FormsyInjectedProps<InputValueType> {
+  value: string;
+  onChange: (name: string, value: string) => void;
+  label: string;
+  setValue: (value: string | number) => void;
+  isValid: boolean;
+  isValidValue: (value: string | number) => boolean;
+  isPristine: boolean;
+  name: string;
+  type: string;
+  multiline?: boolean;
+  rowsMax?: number;
+  rows?: number;
+  className?: string;
+  placeholder?: string;
+  errorMessage: string;
+}
+
+const TextInput: FunctionComponent<Props> = ({
+  name,
+  label,
+  type,
+  onChange,
+  multiline,
+  rows,
+  rowsMax,
+  value,
+  setValue,
+  errorMessage,
+  isValid,
+  isPristine,
+  className,
+  placeholder,
+}) => {
+  const changeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(name, event.target.value);
+    setValue(event.target.value);
+  };
+
+  return (
+    <TextField
+      error={!isPristine && !isValid}
+      name={name}
+      type={type || 'text'}
+      value={value}
+      label={label}
+      variant="outlined"
+      multiline={multiline}
+      rows={rows || 1}
+      rowsMax={rowsMax || 1}
+      onChange={changeValue}
+      helperText={isPristine ? null : errorMessage}
+      className={className}
+      placeholder={placeholder || ''}
+    />
+  );
+};
+
+export default withFormsy(TextInput);
