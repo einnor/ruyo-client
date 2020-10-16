b24383176433f6e8c56cfe83e83ba52526cb03bc
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-16 15:54:39 +0300

    Created an Orders API service to fetch all orders and to update an order

---
+++ src/services/Api.ts
@@ -0,0 +1,42 @@
+import { AxiosResponse } from 'axios';
+
+export default class Api {
+  public static handleResponseData(response: AxiosResponse): any {
+    switch (response.status) {
+      case 200:
+      case 201:
+        return response.data;
+
+      // BAD REQUEST
+      case 400:
+        throw {
+          status: response.status,
+          error: 'Bad Request',
+          message: 'The request data is invalid.',
+          body: response,
+        };
+
+      // NOT FOUND
+      case 404:
+        throw {
+          status: response.status,
+          error: 'Not Found',
+          message: 'The resource could not be found',
+        };
+
+      case 500:
+        throw {
+          status: response.status,
+          error: 'Internal Server Error',
+          message: 'There is something wrong with the server at the moment.',
+        };
+
+      default:
+        throw {
+          status: 0,
+          error: 'Unknown Error',
+          message: 'Oops! Please check back in a moment.',
+        };
+    }
+  }
+}
---
+++ src/services/Order.ts
@@ -0,0 +1,24 @@
+import axios from '../plugins/axios';
+import Api from './Api';
+
+export default class Order {
+  public static async fetchAll() {
+    try {
+      // TODO
+      // const response = await axios.get('/orders');
+      // return Api.handleResponseData(response);
+    } catch (error) {
+      return Api.handleResponseData(error.response);
+    }
+  }
+
+  public static async update() {
+    try {
+      // TODO
+      // const response = await axios.put('/orders', order);
+      // return Api.handleResponseData(response);
+    } catch (error) {
+      return Api.handleResponseData(error.response);
+    }
+  }
+}
