7f8df8d2106b3383e841f8be3d92ef9ca5bb695c
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-16 21:11:58 +0300

    Delete order form

---
+++ src/containers/Orders/DeleteOrderForm.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import {
+  Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button,
+} from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import { makeStyles } from '@material-ui/core/styles';
+import { ScreenOrders } from 'i18n/en';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    padding: theme.spacing(2),
+  },
+  appBar: {
+    backgroundColor: theme.palette.primary.contrastText,
+    margin: '-16px 0 20px -20px',
+    width: 'calc(100% + 40px)',
+  },
+  title: {
+    marginBottom: theme.spacing(2),
+    fontSize: theme.spacing(2.5),
+  },
+  buttonWrapper: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    textAlign: 'left',
+  },
+}));
+
+interface IProps {
+  onSubmit: (id: string) => void;
+  isLoading: boolean;
+  id: string;
+  onToggleDrawer?: () => void;
+  order: any;
+}
+
+const DeleteOrderForm = ({
+  id,
+  onSubmit,
+  isLoading = false,
+  onToggleDrawer = () => {},
+  order,
+}: IProps) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar
+        position="sticky"
+        color="transparent"
+        elevation={0}
+        variant="outlined"
+        className={classes.appBar}
+      >
+        <Toolbar
+          className="toolbar"
+          style={{ display: 'flex', justifyContent: 'space-between' }}
+        >
+          <Typography variant="h6">{ScreenOrders.DELETE_TITLE}</Typography>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={onToggleDrawer}
+          >
+            <CloseIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Typography variant="body2" align="left" color="textPrimary">
+        {ScreenOrders.DELETE_DESCRIPTION}
+        &nbsp; ({order?.name})
+      </Typography>
+      <div className={classes.buttonWrapper}>
+        <Button
+          type="button"
+          variant="contained"
+          onClick={() => onSubmit(id)}
+          disabled={isLoading}
+          color="secondary"
+        >
+          {ScreenOrders.DELETE_BUTTON_LABEL}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteOrderForm;
