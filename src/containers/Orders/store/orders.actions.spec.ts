cde04155d907128a0afaab2f1e697b955967caa0
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-17 08:13:11 +0300

    Added unit tests for get order by id actions

--- src/containers/Orders/store/orders.actions.spec.ts
+++ src/containers/Orders/store/orders.actions.spec.ts
@@ -53,4 +53,45 @@ describe('Orders - Actions', () => {
       expect(actionCreator).toEqual(expectedAction);
     });
   });
+
+  describe('Get Order', () => {
+    it('should fetch an order by id', () => {
+      const id = '1';
+      const expectedAction = {
+        type: actions.GET_ORDER_REQUEST,
+        payload: { id },
+      };
+
+      const actionCreator = actions.getOrderRequest(id);
+      expect(actionCreator).toEqual(expectedAction);
+    });
+
+    it('should return payload on success', () => {
+      const payload: Order = {
+        id: '1',
+        title: 'Test',
+        bookingDate: '12/12/2020',
+        address: '86-10300, Kerugoya',
+        customer: 'John Doe',
+      };
+      const expectedAction = {
+        type: actions.GET_ORDER_SUCCESS,
+        payload: payload,
+      };
+
+      const actionCreator = actions.getOrderSuccess(payload);
+      expect(actionCreator).toEqual(expectedAction);
+    });
+
+    it('should return error on failure', () => {
+      const expectedAction = {
+        type: actions.GET_ORDER_FAILURE,
+        payload: { error },
+        error: true,
+      };
+
+      const actionCreator = actions.getOrderFailure(error);
+      expect(actionCreator).toEqual(expectedAction);
+    });
+  });
 });
