55331d0a270592d5a7ebdf80384fc1ed02a72093
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-17 08:10:07 +0300

    Added unit tests for get orders actions

---
+++ src/containers/Orders/store/orders.actions.spec.ts
@@ -0,0 +1,56 @@
+import * as actions from './orders.actions';
+import { APIError } from 'types';
+import { Order } from './orders.types';
+
+describe('Orders - Actions', () => {
+  let error: APIError;
+
+  beforeAll(() => {
+    error = {
+      error: 'Here is an error',
+      message: 'Some error about failure',
+      status: 500,
+    };
+  });
+
+  describe('Get Orders', () => {
+    it('should fetch all orders', () => {
+      const expectedAction = {
+        type: actions.GET_ORDERS_REQUEST,
+      };
+
+      const actionCreator = actions.getOrdersRequest();
+      expect(actionCreator).toEqual(expectedAction);
+    });
+
+    it('should return payload on success', () => {
+      const payload: Order[] = [
+        {
+          id: '1',
+          title: 'Test',
+          bookingDate: '12/12/2020',
+          address: '86-10300, Kerugoya',
+          customer: 'John Doe',
+        },
+      ];
+      const expectedAction = {
+        type: actions.GET_ORDERS_SUCCESS,
+        payload: payload,
+      };
+
+      const actionCreator = actions.getOrdersSuccess(payload);
+      expect(actionCreator).toEqual(expectedAction);
+    });
+
+    it('should return error on failure', () => {
+      const expectedAction = {
+        type: actions.GET_ORDERS_FAILURE,
+        payload: { error },
+        error: true,
+      };
+
+      const actionCreator = actions.getOrdersFailure(error);
+      expect(actionCreator).toEqual(expectedAction);
+    });
+  });
+});
