57b36c0ef6c8d9528db2a079107d57ef7b9bbbbf
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-18 11:31:14 +0300

    Registered watcher

--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -1,5 +1,7 @@
 import { all } from 'redux-saga/effects';
 
+import ordersWatcher from 'containers/Orders/store/orders.sagas';
+
 export default function* rootSaga() {
-  yield all([]);
+  yield all([ordersWatcher()]);
 }
