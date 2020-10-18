f021c13fd46a4149bbc6167e31511f710d0a6adc
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-18 11:43:08 +0300

    Defined saga unit tests for getting an order by id

--- src/containers/Orders/store/orders.sagas.spec.ts
+++ src/containers/Orders/store/orders.sagas.spec.ts
@@ -35,10 +35,43 @@ describe('Orders - Sagas', () => {
         .run();
     });
 
-    it('should handle an unsuccessfully requeste', () => {
+    it('should handle an unsuccessfully request', () => {
       return expectSaga(sagas.getOrdersRequest, action)
         .provide([[matchers.call(OrderApi.fetchAll), Promise.reject(error)]])
         .put(actions.getOrdersFailure(error))
+        .run();
+    });
+  });
+
+  describe('Get Order', () => {
+    const id = '1';
+    const action: FluxStandardAction = {
+      type: actions.GET_ORDER_REQUEST,
+      payload: { id },
+    };
+
+    it('should handle a successful request', () => {
+      const expectedResult: Order = {
+        id: '1',
+        title: 'Test',
+        bookingDate: '12/12/2020',
+        address: '86-10300, Kerugoya',
+        customer: 'John Doe',
+      };
+
+      return expectSaga(sagas.getOrderRequest, action)
+        .provide([[matchers.call(OrderApi.fetchById, id), expectedResult]])
+        .put(actions.getOrderSuccess(expectedResult))
+        .run();
+    });
+
+    it('should handle an unsuccessfully request', () => {
+      const id = '1';
+      return expectSaga(sagas.getOrderRequest, action)
+        .provide([
+          [matchers.call(OrderApi.fetchById, id), Promise.reject(error)],
+        ])
+        .put(actions.getOrderFailure(error))
         .run();
     });
   });
