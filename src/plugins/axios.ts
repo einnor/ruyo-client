b7096d0f380b2ed432e452f9e68ca2ed2a4fc01a
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-19 14:02:27 +0300

    Inject the token in the headers

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,7 +1,22 @@
 import axios from 'axios';
 
-import { get } from '../config';
+import { get } from 'config';
+import firebase from 'services/Firebase';
+
+const getToken = (): string => {
+  firebase
+    .auth()
+    .currentUser.getIdToken(/* forceRefresh */ true)
+    .then((idToken) => {
+      return idToken;
+    })
+    .catch(function (error) {
+      // Handle error
+    });
+  return '';
+};
 
 export default axios.create({
   baseURL: get('REACT_APP_BACKEND_BASE_PATH'),
+  headers: { Authorization: `Bearer ${getToken()}` },
 });
