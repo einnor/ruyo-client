af5aa708c9e167c8ceaa99367b10a65bda1aa9cb
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-19 13:00:40 +0300

    Added unit tests for the authentication selectors

---
+++ src/containers/Authentication/store/authentication.selectors.spec.ts
@@ -0,0 +1,39 @@
+import IGlobalState from 'types';
+import { AuthenticationState } from './authentication.types';
+import * as selectors from './authentication.selectors';
+import { initialState } from './authentication.reducer';
+
+describe('Authentication - Selectors', () => {
+  let state: AuthenticationState;
+
+  beforeAll(() => {
+    state = { ...initialState };
+  });
+
+  it('should return isFetching', () => {
+    const isFetching = selectors.getIsFetching({
+      authentication: state,
+    } as IGlobalState);
+    expect(isFetching).toEqual(state.isFetching);
+  });
+
+  it('should return token', () => {
+    const token = selectors.getToken({ authentication: state } as IGlobalState);
+    expect(token).toEqual(state.token);
+  });
+
+  it('should return uid', () => {
+    const uid = selectors.getUID({ authentication: state } as IGlobalState);
+    expect(uid).toEqual(state.uid);
+  });
+
+  it('should return user data', () => {
+    const data = selectors.getData({ authentication: state } as IGlobalState);
+    expect(data).toEqual(state.data);
+  });
+
+  it('should return error', () => {
+    const error = selectors.getError({ authentication: state } as IGlobalState);
+    expect(error).toEqual(state.error);
+  });
+});
--- src/containers/Orders/store/orders.selectors.spec.ts
+++ src/containers/Orders/store/orders.selectors.spec.ts
@@ -1,3 +1,4 @@
+import IGlobalState from 'types';
 import { OrdersState } from './orders.types';
 import * as selectors from './orders.selectors';
 import { initialState } from './orders.reducer';
@@ -10,17 +11,19 @@ describe('Orders - Selectors', () => {
   });
 
   it('should return isFetching', () => {
-    const isFetching = selectors.getIsFetching({ orders: state });
+    const isFetching = selectors.getIsFetching({
+      orders: state,
+    } as IGlobalState);
     expect(isFetching).toEqual(state.isFetching);
   });
 
-  it('should return image url', () => {
-    const data = selectors.getData({ orders: state });
+  it('should return orders data', () => {
+    const data = selectors.getData({ orders: state } as IGlobalState);
     expect(data).toEqual(state.data);
   });
 
   it('should return error', () => {
-    const error = selectors.getError({ orders: state });
+    const error = selectors.getError({ orders: state } as IGlobalState);
     expect(error).toEqual(state.error);
   });
 });
